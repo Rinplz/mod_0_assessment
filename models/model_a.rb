1a4abbb2ef7816d98ba6b66f1f4776f18e22d71a
Rin Fabian <pfabianschool@gmail.com>
2021-11-12 15:42:17 -0600

    add laptop model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+model_laptop = {
+  brand: 'Lenovo',
+  model: 'Chromebook 3',
+  memory_in_gb: 32,
+  screen_size_inch: 11,
+  has_headphone_jack: true,
+  has_backlit_keyboard: false,
+  sellers: ['Walmart', 'Best Buy', 'Newegg'],
+  usb_types: ['USB A 3.1', 'USB C', 'USB C 3.1']
+}
