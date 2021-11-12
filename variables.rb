3d4cbce81bbb4f2f624e4d607d7fc9c8ddad6fa2
Rin Fabian <pfabianschool@gmail.com>
2021-11-12 15:30:06 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+animal = "koala"
+is_cuddly = true
+
+animal = "alligator"
+
+num_8 = 8
+num_3 = 3
+
+quotient = num_3 / num_8
+
+favorite_films = ['Your Name','A Silent Voice', 'Weathering with You']
+
+favorite_films.shift()
+
+p animal.length()
