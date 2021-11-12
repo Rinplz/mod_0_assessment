0d9c8d01edde95bae7f0613b4c34e98d3ba5ce6c
Rin Fabian <pfabianschool@gmail.com>
2021-11-12 15:48:35 -0600

    add house model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+model_house = {
+  bedrooms: 3,
+  bathrooms: 2.5,
+  has_backyard: true,
+  has_garage: true,
+  types_of_rooms: ['Bedrooms','Bathrooms','Living Room','Kitchen', 'Laundry Room'],
+  color: 'red',
+  address: '1434 Model Road',
+  furniture: ['beds','stove','refrigerator','recliner','sofa','desk','television']
+}
